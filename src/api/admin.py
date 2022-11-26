4d1cd043638d5ddb8f22fb11bb41a7078ac02839
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-26 17:36:17 -0300

    Actualización admin.py

---
+++ src/api/admin.py
@@ -0,0 +1,24 @@
+import os
+from flask_admin import Admin
+from models import db, User
+from flask_admin.contrib.sqla import ModelView
+from wtforms.fields import PasswordField
+
+
+def setup_admin(app):
+    app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+
+    admin.add_view(ModelView(User, db.session))
+
+
+class UserView(ModelView):
+    column_list = ['email', 'is_active']
+    column_editable_list = ['is_active', ]
+    column_exclude_list = ['_password', ]
+    create_modal = True
+    edit_modal = True
+    form_extra_fields = {
+        'password': PasswordField('password')
+    }
