e2fd4078b1d7737bdb94b7fca143df3a755b43e5
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-25 21:14:09 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../src/front/styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello There</h1>
+    </div>
+  );
+}
+
+export default App;
