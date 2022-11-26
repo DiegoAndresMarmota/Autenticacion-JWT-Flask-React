928b058ed40e680e3493b75e29ffefd0ce8349e3
Diego Andres Echalar <diegoechalarp@hotmail.com>
2022-11-25 22:15:16 -0300

    Actualización de appContext

---
+++ src/front/js/store/appContext.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import getState from "../store/flux";
+
+export const Context = React.createContext(null);
+
+const injectContext = (PassedComponent) => {
+  const StoreWrapper = (props) => {
+    const [state, setState] = useState(
+      getState({
+        getStore: () => state.store,
+        getActions: () => state.actions,
+        setStore: (updatedStore) =>
+          setState({
+            store: Object.assign(state.store, updatedStore),
+            actions: { ...state.actions },
+          }),
+      })
+    );
+
+    useEffect(() => {
+      state.actions.getMessage();
+      state.actions.syncTokenFromSessionStore();
+    }, []);
+    return (
+      <Context.Provider value={state}>
+        <PassedComponent {...props} />
+      </Context.Provider>
+    );
+  };
+  return StoreWrapper;
+};
+
+export default injectContext;
